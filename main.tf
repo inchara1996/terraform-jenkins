872487db83a2c274fbe518eff36f5f38e519bb5c
inchara1996 <incharanbhushan1996@gmail.com>
2026-01-12 17:39:17 +0000

    pushing main and provider file

---
+++ main.tf
@@ -0,0 +1,13 @@
+variable v1 {
+    type = string 
+    default = "hello"
+}
+
+resource local_file r1 {
+    filename = "abc.txt"
+    content = var.v1
+}
+
+output o1 {
+    value = local_file.r1.id
+}
